e4dbbf4f11069394968a41d90b2df250cee2e712
Rene Bridgman <renebridgman@Renes-iMac.local>
2023-01-28 15:01:43 +0100

    add new layout for MenuItem.js

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,51 +1,14 @@
 import React from 'react'
 import './MenuItem.css'
 
-export default function MenuItem() {
+export default function MenuItem({items}) {
 
-    const items = [
-        {
-            item: "Chicken Burger",
-            price: 8.99,
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus pulvinar nisi sed commodo. Pellentesque nec mauris varius, vehicula arcu id, aliquam quam. Donec eros neque, fermentum id bibendum mattis, dignissim in dolor. Donec ligula odio, eleifend et egestas in, tristique quis diam. ",
-            imgUrl: "/cheeseburger.webp"
-        },
-        {
-            item: "Veggie Burger",
-            price: 8.99,
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus pulvinar nisi sed commodo. Pellentesque nec mauris varius, vehicula arcu id, aliquam quam. Donec eros neque, fermentum id bibendum mattis, dignissim in dolor. Donec ligula odio, eleifend et egestas in, tristique quis diam. ",
-            imgUrl: "/cheeseburger.webp"
-        },
-        {
-            item: "Curly Fries",
-            price: 3.99,
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus pulvinar nisi sed commodo. Pellentesque nec mauris varius, vehicula arcu id, aliquam quam. Donec eros neque, fermentum id bibendum mattis, dignissim in dolor. Donec ligula odio, eleifend et egestas in, tristique quis diam. ",
-            imgUrl: "/cheeseburger.webp"
-        },
-        {
-            item: "Chicky Nuggies",
-            price: 5.99,
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus pulvinar nisi sed commodo. Pellentesque nec mauris varius, vehicula arcu id, aliquam quam. Donec eros neque, fermentum id bibendum mattis, dignissim in dolor. Donec ligula odio, eleifend et egestas in, tristique quis diam. ",
-            imgUrl: "/cheeseburger.webp"
-        },
-        {
-            item: "Fritz Cola",
-            price: 2.99,
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus pulvinar nisi sed commodo. Pellentesque nec mauris varius, vehicula arcu id, aliquam quam. Donec eros neque, fermentum id bibendum mattis, dignissim in dolor. Donec ligula odio, eleifend et egestas in, tristique quis diam. ",
-            imgUrl: "/cheeseburger.webp"
-        }
-]
-
-    console.log(items[1].item)
 
   return (
-    <>
+    <div className="menu-container">
         {items.map((item) => (
             <React.Fragment key={item.item}>
                 <div className="card">
-                    <div className="card-image">
-                        <img src={item.imgUrl}/>     
-                    </div>
                     <div className="item-text">
                         <h3>{item.item}</h3>
                         <hr></hr>
@@ -53,9 +16,10 @@ export default function MenuItem() {
                         <p>${item.price}</p>
                         <button>Add Item</button>
                     </div>
+                    <img src={item.imgUrl}/>     
                 </div>
             </React.Fragment>
         ))}
-    </>
+    </div>
   )
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,14 +2,46 @@ import './Home.css'
 import MenuItem from '../../components/MenuItem'
 
 export default function Home() {
+
+    const items = [
+        {
+            item: "Chicken Burger",
+            price: 8.99,
+            description: "Lorem ipsum dolor sit amet.",            
+            imgUrl: "/cheeseburger.webp"
+        },
+        {
+            item: "Veggie Burger",
+            price: 8.99,
+            description: "Lorem ipsum dolor sit amet.",            
+            imgUrl: "/cheeseburger.webp"
+        },
+        {
+            item: "Curly Fries",
+            price: 3.99,
+            description: "Lorem ipsum dolor sit amet.",            
+            imgUrl: "/cheeseburger.webp"
+        },
+        {
+            item: "Chicky Nuggies",
+            price: 5.99,
+            description: "Lorem ipsum dolor sit amet.",            
+            imgUrl: "/cheeseburger.webp"
+        },
+        {
+            item: "Fritz Cola",
+            price: 2.99,
+            description: "Lorem ipsum dolor sit amet.",
+            imgUrl: "/cheeseburger.webp"
+        }
+]
     
     return (
-        <div>
+        <div className="content-container">
             <h1>HOME</h1>
             <p>Yummm! Check out our delicious new food!</p>
-            <div className="menu-container">
-                <MenuItem />
-            </div>
+                <MenuItem items={items}/>
         </div>
+        
     )
 }
